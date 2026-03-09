332812a46fa47cbe2ec0973895e6967aeb503858
amrendrasharma1328-a11y <amrendrasharma1328@gmail.com>
2026-03-10 00:33:42 +0530

    routing handling middlewares

--- src/app.js
+++ src/app.js
@@ -4,9 +4,35 @@ const express=require("express");
 const app = express();
 
 
-app.use((req,res)=>{
-    res.send("hello dosto kaise ho")
+app.use("/user",(req,res,next)=>{
+    console.log("response 1 !!")
+    // res.send("this is response 1!!")
+    next();
+},
+(req,res,next)=>{
+    console.log("ayr batao ");
+    // res.send("response 2!!")
+    next();
+})
+
+
+
+// is caseme nhi work krega bacuse xpress kahega ki aage kya hai me get nhi kr pa arha 
+
+
+
+app.get("/user",(req,res,next)=>{
+    console.log("response 1 !!")
+    res.send("this is response 1!!")
+    next();
+})
+app.get("/user",(req,res,next)=>{
+    console.log("response 1 !!")
+    res.send("this is response 2!!")
+    next();
 })
 app.listen(3000,()=>{
     console.log("sucessfull running");
 });
+
+
